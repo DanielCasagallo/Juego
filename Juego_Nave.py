7276630a4096c8240d9e009117f3973c30887aae
ADAM-PC\CLIENTE <bryan.perez01@epn.edu.ec>
2017-07-04 19:42:20 -0500

    Creacion de la nave espacial y la funcion para su movimiento, ademas del cambio de la imagen de fondo para el nuevo tamaño de pantalla

--- Juego_Nave.py
+++ Juego_Nave.py
@@ -1,22 +1,48 @@
 import pygame, sys, os
 from pygame.locals import *
 
-SCREEN_WIDTH = 750
-SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+def teclado():
+    teclado = pygame.key.get_pressed()
+
+    global NaveposX
+
+    if teclado[K_RIGHT]:
+            NaveposX += 5
+
+    if teclado[K_LEFT]:
+            NaveposX -= 5
 
 pygame.init()
 ventana = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Juego")
-fondo = pygame.image.load("fondo.jpg") 
-ventana.blit(fondo,(0,0))
 
+fondo = pygame.image.load("fondo.jpg")
 
+reloj = pygame.time.Clock()
+
+#Creacion y posicion de la nave
+imagen = pygame.image.load("nave.png")
+NaveposX = 500
+#La variable de la posicion Y sera constante ya que esta no variara durante el transcurso del juego
+CNaveposY = 590
+
+#Variables para uso de colores
+blanco = (255, 255, 255)
 
 while True:
+    teclado()
+    ventana.fill(blanco)
+    ventana.blit(fondo, (0, 0))
+    ventana.blit(imagen, (NaveposX, CNaveposY))
+
     for evento in pygame.event.get():
         if evento.type == QUIT:
             pygame.quit()
             sys.exit()
 
+    reloj.tick(60)
     pygame.display.flip()
     pygame.display.update()
