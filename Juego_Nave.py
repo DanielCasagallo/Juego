a29f49c47e49096134af194d5fe9a8d837bda191
Pit_killer2.1 <brad.alvarez@epn.edu.ec>
2017-07-05 23:11:10 -0500

    estan algunos sonidos con la "d" (disparo), "e"(explosion), "m"(la musica q asome en el menu)

--- Juego_Nave.py
+++ Juego_Nave.py
@@ -11,31 +11,48 @@ def teclado():
     global NaveposY
 
     if teclado[K_RIGHT]:
-        if NaveposX <=1032:
+        if NaveposX <=1130:
             NaveposX += 5
 
     if teclado[K_LEFT]:
-        if NaveposX > 0:
+        if NaveposX > -10:
             NaveposX -= 5
             
     if teclado[K_DOWN]:
-        if NaveposY <=590:
+        if NaveposY <=615:
             NaveposY += 5
 
     if teclado[K_UP]:
         if NaveposY > 0:
             NaveposY -= 5
+    if teclado[K_b]:
+        sonido.play()
+    if teclado[K_m]:
+        sonido1.play()
+    if teclado[K_d]:
+        sonido2.play()
+    if teclado[K_e]:
+        sonido3.play()
+        
+        
 
 pygame.init()
 ventana = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Juego")
-
+pygame.mixer.music.load("fondos.mp3")
 fondo = pygame.image.load("fondo.jpg")
+
+sonido = pygame.mixer.Sound("boss.wav")
+sonido1 = pygame.mixer.Sound("menu.wav")
+sonido2 = pygame.mixer.Sound("disparo.wav")
+sonido3 = pygame.mixer.Sound("explosion.wav")
 
 reloj = pygame.time.Clock()
 
 #Creacion y posicion de la nave
-imagen = pygame.image.load("nave.png")
+pygame.mixer.music.play()
+imagen = pygame.image.load("nave1.png")
+imagen = pygame.transform.scale(imagen, (200, 100))
 NaveposX = 500
 #La variable de la posicion Y sera constante ya que esta no variara durante el transcurso del juego
 NaveposY = 590
